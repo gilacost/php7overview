5b47adeb9de953836b3864d4ada23b1fabb86ce2
pepodev <josepgiraltdlacoste@gmail.com>
2015-09-07 21:27:20 +0200

    amazing commit last one

--- src/null-coalesce-operator.php
+++ src/null-coalesce-operator.php
@@ -1,1 +1,13 @@
 <?php
+//nowadays
+
+// just try to uncomment this and se how it works
+//$_GET['name'] = 'Pepo';
+$name = isset($_GET['name'])?$_GET['name']:'nothing there';
+var_dump($name);
+
+// Null Coalesce Operator
+
+$name = $_GET['name'] ?? 'nothing there';
+var_dump($name);
+echo $firstName ?? 'nothing there';
